831032829db58fafb6d4c7b956c69d94a1d6b4b1
Lawrence Meckan <media@absalom.biz>
2015-11-10 14:14:46 +1000

    More PHP7

--- src/sass.c
+++ src/sass.c
@@ -92,15 +92,6 @@ zend_object * sass_create_handler(zend_class_entry *type TSRMLS_DC) {
  
      return &intern->zo;
 }
-
-static inline struct sass_object * php_custom_object_fetch_object(zend_object *obj) {
-      return (struct sass_object *)((char *)obj - XtOffsetOf(struct sass_object, zo));
-}
-
-#define Z_CUSTOM_OBJ_P(zv) php_custom_object_fetch_object(Z_OBJ_P(zv));
-
-  struct sass_object* obj = Z_CUSTOM_OBJ_P(getThis());
-
 #endif
 
 
