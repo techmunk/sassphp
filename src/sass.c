24720f9ca717f5188fafc5fa0f24a65e2fcc2eee
Lawrence Meckan <media@absalom.biz>
2016-02-22 14:18:15 +1000

    More PHP7 free stuff

--- src/sass.c
+++ src/sass.c
@@ -731,7 +731,7 @@ static PHP_MINIT_FUNCTION(sass)
     memcpy(&sass_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
     #if ZEND_MODULE_API_NO > 20131226
     sass_handlers.offset = XtOffsetOf(struct sass_object, zo);
-    sass_handlers.free_obj = sass_free_storage;
+    sass_handlers.free_obj = *sass_free_storage;
     #endif
     sass_handlers.clone_obj = NULL;
 
