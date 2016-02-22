ddc3ebc2cceef7c961f12d4c5ca5694d9dc7eaac
Lawrence Meckan <media@absalom.biz>
2016-02-22 12:57:26 +1000

    More work on object freeing

--- src/sass.c
+++ src/sass.c
@@ -44,7 +44,7 @@ typedef struct sass_object {
 zend_class_entry *sass_ce;
 
 #if PHP_MAJOR_VERSION >= 7
-void sass_free_storage(void *object TSRMLS_DC)
+static void sass_free_storage(void *object)
 {
     sass_object *obj = (sass_object *)object;
     if (obj->include_paths != NULL)
